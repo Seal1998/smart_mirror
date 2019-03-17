409bbb0463a0c8b6c67fa988943680f237a1c13a
Xabab-pc <14582595+Xabab@users.noreply.github.com>
2019-03-17 14:41:57 +0200

    ##

--- app/frames/events.py
+++ app/frames/events.py
@@ -1,5 +1,12 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.stacklayout import StackLayout
 
 
 class Events(BoxLayout):
     pass
+
+class Event(RelativeLayout):
+    pass
+
+
